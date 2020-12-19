d65e8ac5a5ad27f4e5184d14313c5ab2d3f17b94
Robert Pascua <r.pascua@berkeley.edu>
2020-12-18 16:34:40 -0800

    Make option for skipping frames in making animation.

--- nbody/make_animation.py
+++ nbody/make_animation.py
@@ -8,10 +8,14 @@ try:
     time_per_frame = float(sys.argv[2])
 except (IndexError, ValueError):
     time_per_frame = 5
+try:
+    skip_frames = int(sys.argv[3])
+except IndexError:
+    skip_frames = 1
 files = glob.glob(os.path.join(directory, "*.png"))
 files = sorted(files, key=lambda f: int(f[f.rindex("_")+1:-4]))
 frames = []
-for fn in files:
+for fn in files[::skip_frames]:
     frames.append(Image.open(fn))
 
 frames[0].save(
